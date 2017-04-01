dcddc6ee35d0f9c533c6fdb15c3c784e81e9fde0
torkilso <torkil.solheim@gmail.com>
2017-04-01 14:29:57 +0200

    Added css and functionality

--- client/app/app.js
+++ client/app/app.js
@@ -1,132 +1,270 @@
 // @flow
 
 import React from 'react';
-import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import {render} from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
 
 class Menu extends React.Component {
-  render() {
-    return <div>Menu: <a href="/#/">Customers</a></div>;
-  }
+    render() {
+        return <ul className="list-inline">
+        <li><a href="/#/">Customers</a></li>
+        <li><a href="/#/about">About</a></li>
+        </ul>;
+    }
 }
 
 class CustomerService {
-  static instance=null;
-  
-  // Return singleton
-  static get() {
-    if(!CustomerService.instance)
-      CustomerService.instance=new CustomerService();
-    return CustomerService.instance;
-  }
-  
-  getCustomers() {
-    return fetch("/customers").then((response)=>{
-      if(!response.ok) {
-        throw response.statusText;
-      }
-      return response.json();
-    });
-  }
-  
-  getCustomer(customerId) {
-    return fetch("/customers/"+customerId).then((response)=>{
-      if(!response.ok) {
-        throw response.statusText;
-      }
-      return response.json();
-    });
-  }
-  
-  addCustomer(name, city) {
-    var body=JSON.stringify({name: name, city: city});
-    return fetch("/customers", {method: "POST", headers: new Headers({'Content-Type': 'application/json'}), body: body}).then((response)=>{
-      if(!response.ok) {
-        throw response.statusText;
-      }
-      return response.json();
-    });
-  }
+    static instance = null;
+    lastId = 0;
+
+    customers = [];
+
+    // Return singleton
+    static get() {
+        if (!this.instance)
+            this.instance = new CustomerService();
+        return this.instance;
+    }
+
+    constructor() {
+    }
+
+    getCustomers() {
+      return fetch('/api/customers')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+
+
+    getCustomer(customerId) {
+
+      return fetch('/api/customers/'+customerId)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        return responseJson;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+
+    addCustomer(name, city) {
+        return new Promise((resolve, reject) => {
+            if (name && city) {
+                this.customers.push({id: ++this.lastId, name: name, city: city});
+                resolve(this.lastId);
+                return;
+            }
+            reject("name or city empty");
+        });
+    }
+
+    getIndex(id) {
+        for (var i = 0; i < this.customers.length; i++) {
+            if (this.customers[i].id === id) {
+                return i;
+            }
+        }
+        return -1; //to handle the case where the value doesn't exist
+    }
+
+    delCustomer(id) {
+        return new Promise((resolve, reject) => {
+            if (id) {
+                var index = this.getIndex(id);
+                this.customers.splice(index, 1);
+
+                resolve(this.lastId);
+                return;
+            }
+            reject("id not found");
+        });
+    }
+
+    updateCustomer(id, name, city) {
+        var index = this.getIndex(id);
+        return new Promise((resolve, reject) => {
+            if (name && city) {
+                this.customers[index].name = name;
+                this.customers[index].city = city;
+                resolve(this.lastId);
+                return;
+            }
+            reject("Could not update! Something went wrong :(");
+        });
+    }
 }
 
 class CustomerListComponent extends React.Component {
-  state={status: "", customers: [], newCustomerName: "", newCustomerCity: ""}
-  
-  constructor() {
-    super();
-    
-    CustomerService.get().getCustomers().then((result)=>{
-      this.setState({status: "successfully loaded customer list", customers: result});
-    }).catch((reason)=>{
-      this.setState({status: "error: "+reason});
-    });
-  }
-  
-  // Event methods, which are called in render(), are declared as properties:
-  onNewCustomerFormChanged = (event) => {
-    this.setState({[event.target.name]: event.target.value});
-  }
-  
-  // Event methods, which are called in render(), are declared as properties:
-  onNewCustomer = (event) => {
-    event.preventDefault();
-    var name=this.state.newCustomerName;
-    var city=this.state.newCustomerCity;
-    CustomerService.get().addCustomer(name, city).then((result)=>{
-      this.state.customers.push({id: result, name: name, city});
-      this.setState({status: "successfully added new customer", customers: this.state.customers, newCustomerName: "", newCustomerCity: ""});
-    }).catch((reason)=>{
-      this.setState({status: "error: "+reason});
-    });
-  }
-  
-  render() {
-    var listItems = this.state.customers.map((customer) => 
-      <li key={customer.id}><a href={"/#/customer/"+customer.id}>{customer.name}</a></li>
-    );
-    return <div>status: {this.state.status}<br/><ul>{listItems}</ul>
-        <form onSubmit={this.onNewCustomer} onChange={this.onNewCustomerFormChanged}>
-          <label>Name:<input type="text" name="newCustomerName" value={this.state.newCustomerName} /></label>
-          <label>City:<input type="text" name="newCustomerCity" value={this.state.newCustomerCity} /></label>
-          <input type="submit" value="New Customer"/>
-        </form>
-      </div>
-  }
+    state = {status: "", customers: [], newCustomerName: "", newCustomerCity: "", deleteCustomerId: 0}
+
+    constructor() {
+        super();
+
+        CustomerService.get().getCustomers().then((result) => {
+            this.setState({status: "successfully loaded customer list", customers: result});
+        }).catch((reason) => {
+            this.setState({status: "error: " + reason});
+        });
+    }
+
+    // Event methods, which are called in render(), are declared as properties:
+    onNewCustomerFormChanged = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    getIndex(id) {
+        for (var i = 0; i < this.state.customers.length; i++) {
+             if (this.state.customers[i].id == id) {
+                return i;
+            }
+        }
+        return -1; //to handle the case where the value doesn't existasd
+    }
+
+    // Event methods, which are called in render(), are declared as properties:
+    onNewCustomer = (event) => {
+        event.preventDefault();
+        var name = this.state.newCustomerName;
+        var city = this.state.newCustomerCity;
+        CustomerService.get().addCustomer(name, city).then((result) => {
+            this.state.customers.push({id: result, name: name, city});
+            this.setState({
+                status: "successfully added new customer",
+                customers: this.state.customers,
+                newCustomerName: "",
+                newCustomerCity: ""
+            });
+        }).catch((reason) => {
+            this.setState({status: "error: " + reason});
+        });
+    }
+
+    deleteCustomer = (event) => {
+        event.preventDefault();
+        var id = this.state.deleteCustomerId;
+        CustomerService.get().delCustomer(id).then((result) => {
+
+            var index = this.getIndex(id);
+            if (index != -1) {
+                this.state.customers.splice(index, 1);
+
+                this.setState({
+                    status: "successfully deleted customer",
+                    customers: this.state.customers,
+                    deleteCustomerId: ""
+                });
+            }
+        }).catch((reason) => {
+            this.setState({status: "error: " + reason});
+        });
+    }
+
+    render() {
+        var listItems = this.state.customers.map((customer) =>
+            <a href={"/#/customer/"+customer.id} className="list-group-item" key={customer.id}>{customer.name}</a>
+        );
+        return <div>status: {this.state.status}<br/>
+            <div className="list-group">{listItems}</div>
+            <form onSubmit={this.onNewCustomer} onChange={this.onNewCustomerFormChanged}>
+                <label>Name:<input type="text" name="newCustomerName" value={this.state.newCustomerName} className="form-control"/></label>
+                <label>City:<input type="text" name="newCustomerCity" value={this.state.newCustomerCity}className="form-control"/></label>
+                <input type="submit" value="New Customer" className="btn-success"/>
+            </form>
+            <form onSubmit={this.deleteCustomer} onChange={this.onNewCustomerFormChanged}>
+                <label>Id:<input type="number" name="deleteCustomerId" value={this.state.deleteCustomerId} className="form-control"/></label>
+                <input type="submit" value="Delete customer" className="btn-danger"/>
+            </form>
+        </div>
+    }
 }
 
 class CustomerDetailsComponent extends React.Component {
-  state={status: "", customer: {}}
-  
-  constructor(props) {
-    super(props);
-    
-    CustomerService.get().getCustomer(props.params.customerId).then((result)=>{
-      this.setState({status: "successfully loaded customer details", customer: result});
-    }).catch((reason)=>{
-      this.setState({status: "error: "+reason});
-    });
-  }
-  
-  render() {
-    return <div>status: {this.state.status}<br/>
-      <ul>
-        <li>name: {this.state.customer.name}</li>
-        <li>city: {this.state.customer.city}</li>
-      </ul>
-    </div>
-  }
+    state = {status: "", customer: {}, updateName: "", updateCity: ""}
+
+    constructor(props) {
+        super(props);
+
+        CustomerService.get().getCustomer(props.params.customerId).then((result) => {
+          console.log(result);
+            this.setState({status: "successfully loaded customer details", customer: result});
+        }).catch((reason) => {
+            this.setState({status: "error: " + reason});
+        });
+    }
+
+    updateCustomer = (event) => {
+        event.preventDefault();
+
+        var name = this.state.updateName;
+        var city = this.state.updateCity;
+
+        if (name === "") name = this.state.customer.name;
+        if (city === "") city = this.state.customer.city;
+
+        CustomerService.get().updateCustomer(this.state.customer.id, name, city).then((result) => {
+
+            this.setState({
+                status:"Customer updated",
+                customer:{id:this.state.customer.id, name:name, city:city},
+                updateName:"",
+                updateCity:""
+            })
+
+
+        })
+    }
+
+    onUpdateCustomerChanged = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    render() {
+        return <div><div>status: {this.state.status}</div><br/>
+            <ul className="list-group">
+                <li className="list-group-item">Name: {this.state.customer.name}</li>
+                <li className="list-group-item">City: {this.state.customer.city}</li>
+            </ul>
+            <form onSubmit={this.updateCustomer} onChange={this.onUpdateCustomerChanged}>
+                <label>Name:<input type="text" name="updateName" value={this.state.updateName} className="form-control"/></label>
+                <label>City:<input type="text" name="updateCity" value={this.state.updateCity} className="form-control"/></label>
+                <input type="submit" value="Update customer" className="btn-info right"/>
+            </form>
+        </div>
+
+    }
+}
+
+class AboutComponent extends React.Component {
+    constructor(){
+        super();
+    }
+
+    render(){
+        return <div>
+            <p>Applikasjon for å holde oversikt over kunder!</p>
+            <p>Vi er studenter fra Trondheim </p>
+        </div>
+    }
 }
 
 class Routes extends React.Component {
-  render() {
-    return <Router history={hashHistory}>
-      <Route exact path="/" component={CustomerListComponent}/>
-      <Route path="/customer/:customerId" component={CustomerDetailsComponent}/>
-    </Router>;
-  }
+    render() {
+        return <Router history={hashHistory}>
+            <Route exact path="/" component={CustomerListComponent}/>
+            <Route path="/customer/:customerId" component={CustomerDetailsComponent}/>
+            <Route path="/about" component={AboutComponent}/>
+        </Router>;
+    }
 }
 
-render(<div>
-  <Menu/>
-  <Routes/>
+render(<div className="container">
+    <Menu/>
+    <div><Routes/></div>
 </div>, document.getElementById('root'));
--- server/server.js
+++ server/server.js
@@ -1,63 +1,41 @@
 var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
 
-var nextCustomerId=1;
-class Customer {
-  constructor(name, city) {
-    if(name!="") {
-      this.id = nextCustomerId++;
-      this.name=name;
-      this.city=city;
-    }
-  }
-}
+var app = express();
 
 //This resource makes it possible to download and start the React client
 app.use(express.static(__dirname + "/../client"));
 
-//Make app automatically parse json content
-app.use(bodyParser.json());
+var lastId = 0;
 
-var customers=[];
-customers.push(new Customer("Ola", "Trondheim"));
-customers.push(new Customer("Kari", "Oslo"));
-customers.push(new Customer("Per", "Tromsø"));
+var customers = [
+  {"id":++lastId,"name":"Ola","city":"Trondheim"},
+  {"id":++lastId,"name":"Kari","city":"Oslo"},
+  {"id":++lastId,"name":"Per","city":"Tromsø"}
+];
 
+app.get('/api/customers',function (req, res) {
+  res.send(customers);
+})
 
-//Get all customers
-app.get('/customers', (request, response) => {
-  //sending for instance: [{"id":1,"name":"Ola"}, {"id":2,"name":"Kari"}, {"id":3,"name":"Per"}]
-  var customer_id_and_names=[]
-  for(var c=0;c<customers.length;c++) {
-    customer_id_and_names.push({id: customers[c].id, name: customers[c].name});
-  }
-  response.send(customer_id_and_names);
-});
-
-//Get one customer given its id
-app.get('/customers/:id', (request, response) => {
-  for(var c=0;c<customers.length;c++) {
-    if(customers[c].id==request.params.id) {
-      response.send(customers[c]);
+app.get('/api/customers/:id',function (req, res) {
+  let id = req.params.id;
+  for(var i = 0; i < customers.length; i++){
+    if(customers[i].id == id){
+      res.send(customers[i]);
       return;
     }
   }
-  //Respond with not found status code
-  response.sendStatus(404);
-});
+})
 
-//Add new customer if name and city contain data
-app.post('/customers', (request, response) => {
-  if(request.body.name && request.body.city) {
-    customers.push(new Customer(request.body.name, request.body.city));
-    response.send(customers[customers.length-1].id.toString());
-    return;
-  }
-  //Respond with bad request status code
-  response.sendStatus(400);
-});
+app.post('/api/add',function (req, res) {
 
+})
+
+app.delete('/api/del',function (req, res) {
+
+})
+
+console.log(customers);
 //Start the web server
 //Open for instance http://localhost:3000 in a web browser
 var server = app.listen(3000, () => {
